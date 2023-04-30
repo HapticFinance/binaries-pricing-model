1ed6c220e61fabc9ff2a9a9bdaaf068420e13ad3
alberthaptic <albert@haptic.finance>
2023-04-30 14:04:49 -0400

    rename/ add pdf, edit title, etc

--- gen.r
+++ gen.r
@@ -2,7 +2,7 @@
 path = getwd()
 source(paste(path, "/install.r", sep=""))
 
-rmarkdown::render("gen_wp.rmd",
-                  output_file = glue::glue("pdf/out.pdf"))
+rmarkdown::render("gen.rmd",
+                  output_file = glue::glue("pdf/binaries_valuation.pdf"))
  
 
