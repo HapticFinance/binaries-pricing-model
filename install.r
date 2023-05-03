f365e732b862e9aad0f7c12923c8412920f81b34
alberthaptic <albert@haptic.finance>
2023-05-02 20:05:33 -0400

    cleanup

--- install.r
+++ install.r
@@ -3,12 +3,6 @@ packages <- c(
   "rmarkdown"
 )
 
-# suppressPackageStartupMessages(library(dplyr))
-# suppressPackageStartupMessages(library(tictoc))
-# suppressPackageStartupMessages(library(purrr))
-# suppressPackageStartupMessages(library(jsonlite))
-# suppressPackageStartupMessages(library(pracma))
-
 # Install packages not yet installed
 installed_packages <- packages %in% rownames(installed.packages())
 if (any(installed_packages == FALSE)) {
